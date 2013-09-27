364cd337171eb5e17a3280c2cd72fefbd8e9c5d9
masterr <chguenter@web.de>
2013-09-27 13:20:14 +0200

    125 first solution

--- Euler_125/euler_125.rb
+++ Euler_125/euler_125.rb
@@ -12,31 +12,48 @@ class Integer
 		return result.sort.uniq
 	end
 
+	@@square_memo = {}
+
 	def is_sum_of_cons_squares
-		(1..self).to_a.each do |x| 
-			return true if (x..self).to_a.square == self
-			(x..self).to_a.each { |y| return true if (x..y).to_a.square == self }
+		sq = (Math.sqrt(self)).to_i
+		if @@square_memo.has_key?(self)
+			puts "#{self} => via Memo"
+			return true
 		end
+		(1..sq).each do |x| 
+			(x..sq).each do |y| 
+				arr = (x..y).to_a
+				square = arr.square
+				if square == self && arr.length > 1
+					puts "#{self} => #{arr}"
+					return true 
+				elsif arr.length > 1 && square.is_palin?
+					@@square_memo[square] = true
+				end
+			end
+		end
+		#puts @@square_memo
 		return false
-	end
+	end		
 end
 
-class Array
+class Array	
+	@@memo = {}
+
 	def square
-		self.inject(0) { |mem, var| mem + ( var * var)}	
+		self.inject(0) { |mem, var| mem + ( var * var)}			
 	end
 end
 
 class Euler_125
 	def find_cons_square_palins_under(limit)
 		result = []
-		(1..limit).to_a.each do |x| 
+		(1..limit).each do |x| 
 			next unless x.is_palin?
-			puts x
 			if x.is_sum_of_cons_squares
-				puts "match: #{x}"
 				result << x
 			end
 		end
+		return result
 	end
 end
--- Euler_125/test_euler_125.rb
+++ Euler_125/test_euler_125.rb
@@ -21,7 +21,8 @@ class Test_Integer < Test::Unit::TestCase
 		TEST_CONS_SEQUENCES.each{|input, output| assert_equal(output, input.cons_sequences)}		
 	end
 
-	TEST_CONS = { 595 => true,
+	TEST_CONS = { 1000 => false,
+								595 => true,
 						    594 => false}
 
 	def test_is_sum_of_cons_squares
@@ -44,6 +45,14 @@ class Test_Euler_125 < Test::Unit::TestCase
 	end
 
 	def test_find_cons_square_palins_under
-		assert_equal(0,@euler_125.find_cons_square_palins_under(1000))
+		#assert_equal([5, 55, 77, 181, 313, 434, 505, 545, 595, 636, 818 ],@euler_125.find_cons_square_palins_under(1000))
+	end
+
+	def test_sum
+		assert_equal(false, 100000.is_sum_of_cons_squares)
+		assert_equal(4164, @euler_125.find_cons_square_palins_under(1000).inject(:+))
+		assert_equal(20268, @euler_125.find_cons_square_palins_under(10000).inject(:+))
+		assert_equal(272059, @euler_125.find_cons_square_palins_under(50000).inject(:+))
+		#assert_equal(272059, @euler_125.find_cons_square_palins_under(10**8).inject(:+))
 	end
 end
